49b95d234231e403d7df52a79c9ae79e4d5325be
Przemyslaw Rafalczuk <106072319+prz-raf@users.noreply.github.com>
2023-11-22 12:52:41 +0000

    Update search.py

--- search.py
+++ search.py
@@ -1,5 +1,31 @@
 from typing import Optional
 
+def binary_interpolation_search(search_value: int, search_collection: list) -> Optional[int]:
+    if not search_collection:
+        return None
+
+    start = 0
+    end = len(search_collection) - 1
+    while start <= end:
+        pos = start + \
+              ((search_value - search_collection[start])//(search_collection[end] - search_collection[start])) * \
+              (end - start)
+
+        if pos < start:
+            pos = start
+        elif pos > end:
+            pos = end
+
+        pos_number = search_collection[pos]
+        if pos_number == search_value:
+            return pos
+        elif pos_number < search_value:
+            start = pos + 1
+        else:
+            end = pos - 1
+
+    return None
+
 def binary_search(search_value: int, search_collection: list) -> Optional[int]:
     if not search_collection:
         return None
